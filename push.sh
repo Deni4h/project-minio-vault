5f1264e6c490591be68cb9c0e156098994201cf3
deni4h <denisatria4734@gmail.com>
2025-06-17 09:52:09 +0000

    fix

--- push.sh
+++ push.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-echo "# project-minio-vault" >> README.md
-git init
+
 git add .
-git commit -m "first commit"
-git branch -M main
-git remote add origin git@github.com:Deni4h/project-minio-vault.git
-git push -u origin main
+git commit -m "fix"
+git push origin main
